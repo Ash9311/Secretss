a45da710e8a6b466f3eacc36996bd099e089eefd
@Ash9311 <ashutoshmulki7@gmail.com>
2023-05-24 23:52:51 +0530

    database encrpytion

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
-const mongoose =  require("mongoose")
+const mongoose =  require("mongoose");
+const encrypt = require("mongoose-encryption")
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -12,11 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});  
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email:String,
     password:String
-};
-
+});
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
@@ -50,6 +52,7 @@ app.post("/login",function(req,res){
   User.findOne({email:username}).then(foundUser=>{
         if(foundUser){
             if(foundUser.password===password){
+                console.log(foundUser.password)
                 res.render("secrets");
             }
         }
